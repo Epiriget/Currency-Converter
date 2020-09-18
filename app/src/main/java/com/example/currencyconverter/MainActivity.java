80a8d69456cd3d4e5f496e4167231b9b547d2ddb
Epiriget <epiriget28@gmail.com>
2020-09-18 20:37:56 +0700

    Implemented rough draft of convertation activity

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Spinner spinnerFrom;
+    private Spinner spinnerTo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinnerFrom = findViewById(R.id.spinner_from);
+        spinnerTo = findViewById(R.id.spinner_to);
+        ArrayAdapter<CharSequence> adapterFrom = ArrayAdapter
+                .createFromResource(this, R.array.currency_array,
+                        android.R.layout.simple_spinner_item);
+        adapterFrom.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerFrom.setAdapter(adapterFrom);
+        spinnerTo.setAdapter(adapterFrom);
     }
 }
