2bc278dc14991a24e2b70fc9b29bf06af7edc772
Epiriget <epiriget28@gmail.com>
2020-09-23 22:36:26 +0700

    Implemented json parsing and saving to file

---
+++ app/src/main/java/com/example/currencyconverter/Converter.java
@@ -0,0 +1,14 @@
+package com.example.currencyconverter;
+
+import android.content.Context;
+
+public class Converter {
+    private Context mContext;
+    public Converter(Context context) {
+        mContext = context;
+    }
+
+    public double convert(String from, String to) {
+        return 0.0;
+    }
+}
--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,11 +3,32 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Iterator;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +45,14 @@ public class MainActivity extends AppCompatActivity {
         final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(),
                 tabLayout.getTabCount());
         viewPager.setAdapter(adapter);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new ExchangeRateFetcher().execute();
+            }
+        });
 
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener(){
@@ -42,6 +71,112 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        new ExchangeRateFetcher().execute();
+
+    }
+
+    public class ExchangeRateFetcher extends AsyncTask<Void, Void, String> {
+        private static final String BANK_URL = "https://www.cbr-xml-daily.ru/daily_json.js";
+        private static final String FILENAME = "currentExchangeRate";
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            String tmp = getRateInfo();
+            return getRateInfo();
+        }
+
+        private String getRateInfo() {
+            HttpsURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String rateJSONString = null;
+
+            try {
+                URL requestURL = new URL(BANK_URL);
+
+                urlConnection = (HttpsURLConnection) requestURL.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder builder = new StringBuilder();
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                    builder.append("\n");
+                }
+                if (builder.length() == 0) {
+                    return null;
+                }
+                rateJSONString = builder.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return rateJSONString;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            StringBuilder output = new StringBuilder();
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                JSONObject valuteObject = jsonObject.getJSONObject("Valute");
+                String charCode, nominal, value, name;
+
+                for (Iterator<String> it = valuteObject.keys(); it.hasNext(); ) {
+                    String key = it.next();
+                    JSONObject item = valuteObject.getJSONObject(key);
+                    charCode = item.getString("CharCode");
+                    nominal = item.getString("Nominal");
+                    value = item.getString("Value");
+                    name = item.getString("Name");
+
+                    int intNominal = Integer.parseInt(nominal);
+                    double valuePerNote = Double.parseDouble(value) / intNominal;
+
+                    output.append(String.format("%s %s %f\n", charCode, name, valuePerNote));
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+            FileOutputStream outputStream;
+            try {
+                outputStream = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+                outputStream.write(output.toString().getBytes());
+                outputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            FileInputStream inputStream;
+            try {
+                inputStream = openFileInput(FILENAME);
+                byte[] buffer = new byte[inputStream.available()];
+                inputStream.read(buffer);
+                String str = new String(buffer);
+                inputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            super.onPostExecute(s);
+        }
     }
 }
